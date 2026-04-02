b181f14de3742257f16b8405379e36dc84639f68
Freezy539 <183653226+Freezy539@users.noreply.github.com>
2026-04-03 01:22:51 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 const CONFIG = {
-  apiUrl: "https://script.google.com/macros/s/AKfycbxQSDPhMdJbyaaeeHd-92l8jWqecDdVEkZp_7AOLmIrAnrQeYRyQBrAMElzsGEtAt_F/exec"
+  apiUrl: "https://script.google.com/macros/s/AKfycbxZrQ7Z0ssrOMstxKEliOBeMoIaR5tdQxqWEflEpSyjNKFNAm3FexWNEfrrK-R07ePh/exec"
 };

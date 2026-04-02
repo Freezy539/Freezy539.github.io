c27c1fe9ad183d507388a198da12d98790852f96
Freezy539 <183653226+Freezy539@users.noreply.github.com>
2026-04-03 01:38:32 +0300

    Revert "Update config.js"
    This reverts commit 2f5116dab586ccf79086b1b2896837a4deb031f1.

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 const CONFIG = {
-  apiUrl: "https://script.google.com/macros/s/AKfycbxQSDPhMdJbyaaeeHd-92l8jWqecDdVEkZp_7AOLmIrAnrQeYRyQBrAMElzsGEtAt_F/exec"
+  apiUrl: "https://script.google.com/macros/s/AKfycbx93_-hbqQHwMsV6jDgnTa8P_oLShN0KDRr9zH9UuJgifxIAitiLal9MNvvEcgfNW7E/exec"
 };

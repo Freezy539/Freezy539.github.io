ebf17edefe474d174e20d97b4d4fae8a53febd73
Freezy539 <183653226+Freezy539@users.noreply.github.com>
2026-04-03 01:38:43 +0300

    Revert "a"
    This reverts commit be7c6193d325883ff36859b0885fa0727a7aa989.

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 const CONFIG = {
-  apiUrl: "https://script.google.com/macros/s/AKfycbx93_-hbqQHwMsV6jDgnTa8P_oLShN0KDRr9zH9UuJgifxIAitiLal9MNvvEcgfNW7E/exec"
+  apiUrl: "PASTE_YOUR_APPS_SCRIPT_WEB_APP_URL_HERE"
 };

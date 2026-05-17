2b604823a64141d8cacb775d44b533b276eb94b7
Freezy539 <183653226+Freezy539@users.noreply.github.com>
2026-05-17 13:29:21 +0300

    a

--- config.js
+++
@@ -1,5 +0,0 @@
-const CONFIG = {
-  // Pane siia Apps Script Web App URL.
-  // Näide: https://script.google.com/macros/s/AKfycbxxxxxxxxxxxxxxxx/exec
-  apiUrl: "https://script.google.com/macros/s/AKfycbx93_-hbqQHwMsV6jDgnTa8P_oLShN0KDRr9zH9UuJgifxIAitiLal9MNvvEcgfNW7E/exec"
-};
